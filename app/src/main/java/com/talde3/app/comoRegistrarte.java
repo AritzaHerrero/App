d00cd1717a1a9a6216fc7225cbbbcc8e8d3c7903
in2dm3 <in2dm3@elorrieta.int>
2023-09-21 12:08:01 +0200

    FireBase-kin konektatu

---
+++ app/src/main/java/com/talde3/app/comoRegistrarte.java
@@ -0,0 +1,14 @@
+package com.talde3.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class comoRegistrarte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_como_registrarte);
+    }
+}
