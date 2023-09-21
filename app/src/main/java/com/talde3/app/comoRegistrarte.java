53b4283093a9ef094c81fb0ab1449c4309461686
in2dm3 <in2dm3@elorrieta.int>
2023-09-21 14:21:34 +0400

    Terminando lo de hoy

--- app/src/main/java/com/talde3/app/comoRegistrarte.java
+++ app/src/main/java/com/talde3/app/comoRegistrarte.java
@@ -2,7 +2,11 @@ package com.talde3.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class comoRegistrarte extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class comoRegistrarte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_como_registrarte);
+
+        Button comoEmpresa = findViewById(R.id.btnComoEmpresa);
+        comoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginORegistro.this, primeraPantalla.class);
+                startActivity(intent);
+            }
+        });
+
+        Button comoI = findViewById(R.id.btnComoEmpresa);
+        comoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginORegistro.this, primeraPantalla.class);
+                startActivity(intent);
+            }
+        });
+
+        Button comoEmpresa = findViewById(R.id.btnComoEmpresa);
+        comoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginORegistro.this, primeraPantalla.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
