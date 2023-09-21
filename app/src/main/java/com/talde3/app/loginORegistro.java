673eb96d4e01d9149e3b170aa1924d74f6e8ed6a
in2dm3 <in2dm3@elorrieta.int>
2023-09-21 11:52:55 +0200

    -loginORegistro.java: Intent a comoRegistrarte mediante txtRegistrarseLogin

--- app/src/main/java/com/talde3/app/loginORegistro.java
+++ app/src/main/java/com/talde3/app/loginORegistro.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class loginORegistro extends AppCompatActivity {
 
@@ -16,10 +17,19 @@ public class loginORegistro extends AppCompatActivity {
 
 
         Button loginButton = findViewById(R.id.btnLoginLogin);
+        TextView txtRegistrarseLogin = findViewById(R.id.txtRegistrarseLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(loginORegistro.this, primeraPantalla.class);
+                startActivity(intent);
+            }
+        });
+
+        txtRegistrarseLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginORegistro.this, comoRegistrarte.class);
                 startActivity(intent);
             }
         });
