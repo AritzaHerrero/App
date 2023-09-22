d2274b9f88850a270693cac11beeef61642a0643
in2dm3 <markel.seabrookebi@elorrieta-errekamari.com>
2023-09-22 14:14:31 +0400

    -Primeros pasos para lectura y escritura del apartado de registro de usuarios de la base de datos.
    -Correcciones varias

--- app/src/main/java/com/talde3/app/registro_como_empresa.java
+++ app/src/main/java/com/talde3/app/registro_como_empresa.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class registro_como_empresa extends AppCompatActivity {
 
@@ -13,6 +14,14 @@ public class registro_como_empresa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_como_empresa);
+
+        EditText NombreEmpresa = findViewById(R.id.etRegistroEmpresaNombre);
+        EditText DireccionEmpresa = findViewById(R.id.etRegistroEmpresaDireccion);
+        EditText NIFEmpresa = findViewById(R.id.etRegistroEmpresaNIF);
+        EditText TelefonoEmpresa = findViewById(R.id.etRegistroEmpresaTelefono);
+        EditText EmailEmpresa = findViewById(R.id.etRegistroEmpresaEmail);
+        EditText ContrasenaEmpresa = findViewById(R.id.etRegistroEmpresaContrasena);
+
         Button btnRetroceder = findViewById(R.id.btnRegistroEmpresaRetroceder);
         Button btnRegistrar = findViewById(R.id.btnRegistroEmpresaRegistrar);
 
