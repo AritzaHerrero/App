fb12cb8c54e091d5deb046e7f96c672802e6470e
in2dm3 <markel.seabrookesbi@elorrieta-errekamari.com>
2023-09-25 12:22:54 +0200

    -Reinvención total del registro de empresas, tnato java como xml -Borrado de los primeros pasos de acceso a datos del previo commit

--- app/src/main/java/com/talde3/app/registro_como_empresa_2.java
+++ app/src/main/java/com/talde3/app/registro_como_empresa_2.java
@@ -6,28 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class registro_como_individuo extends AppCompatActivity {
+public class registro_como_empresa_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro_como_individuo);
+        setContentView(R.layout.activity_registro_como_empresa2);
 
-        Button btnRetroceder = findViewById(R.id.btnRegistroIndividuoRetroceder);
-        Button btnRegistrar = findViewById(R.id.btnRegistroIndividuoRegistrar);
+        Button btnRetroceder = findViewById(R.id.btnRegistroEmpresaRetroceder);
+        Button btnSiguiente = findViewById(R.id.btnRegistroEmpresaSiguiente);
+
         btnRetroceder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(registro_como_individuo.this, comoRegistrarte.class);
+                Intent intent = new Intent(registro_como_empresa_2.this, registro_como_empresa.class);
                 startActivity(intent);
             }
         });
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(registro_como_individuo.this, primeraPantalla.class);
+                Intent intent = new Intent(registro_como_empresa_2.this, registro_como_empresa_3.class);
                 startActivity(intent);
             }
             //POR IMPLEMENTAR - Bloqueo de boton frente a parametros vacios o insufucientes/guardado en la base de datos de ususarios nuevos/verfificación de usuario existente
