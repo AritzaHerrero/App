d56036a52c866b51f7c66d020f424b3137dda81d
Aritza <aritz.herreroni@elorrieta-errekamari.com>
2023-10-04 13:42:37 +0200

    Erregistro pantailak eginda, bai empresa bezala eta indibiduala ere bai.

--- app/src/main/java/com/talde3/app/registro_como_empresa.java
+++ app/src/main/java/com/talde3/app/registro_como_empresa.java
@@ -16,7 +16,6 @@ public class registro_como_empresa extends AppCompatActivity {
         setContentView(R.layout.activity_registro_como_empresa);
 
         Button btnRetroceder = findViewById(R.id.btnRegistroEmpresaRetroceder);
-        Button btnSiguiente = findViewById(R.id.btnRegistroEmpresaSiguiente);
 
         btnRetroceder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,14 +23,6 @@ public class registro_como_empresa extends AppCompatActivity {
                 Intent intent = new Intent(registro_como_empresa.this, comoRegistrarte.class);
                 startActivity(intent);
             }
-        });
-        btnSiguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(registro_como_empresa.this, primeraPantalla.class);
-                startActivity(intent);
-            }
-            //POR IMPLEMENTAR - Bloqueo de boton frente a parametros vacios o insufucientes/guardado en la base de datos de ususarios nuevos/verfificación de usuario existente
         });
     }
 }
--- app/src/main/java/com/talde3/app/registro_como_individuo.java
+++ app/src/main/java/com/talde3/app/registro_como_individuo.java
@@ -16,32 +16,5 @@ public class registro_como_individuo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_como_individuo);
-
-        TextView termsAndConditionsLink = findViewById(R.id.txtTerminosYCondiciones);
-        termsAndConditionsLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(registro_como_individuo.this, Terminos_y_condiciones.class);
-                startActivity(intent);
-            }
-        });
-
-        Button btnRetroceder = findViewById(R.id.btnRegistroIndividuoRetroceder);
-        Button btnRegistrar = findViewById(R.id.btnRegistroIndividuoRegistrar);
-        btnRetroceder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(registro_como_individuo.this, comoRegistrarte.class);
-                startActivity(intent);
-            }
-        });
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(registro_como_individuo.this, primeraPantalla.class);
-                startActivity(intent);
-            }
-            //POR IMPLEMENTAR - Bloqueo de boton frente a parametros vacios o insufucientes/guardado en la base de datos de ususarios nuevos/verfificación de usuario existente
-        });
     }
 }
