96b40d6b90f6c4b0305c827d8304f7b7cf834a44
Aingeru <aingerudaniel.siranaulasa@elorrieta-errekamari.com>
2023-10-06 12:01:19 +0200

    Menua bistaratu/ezkutatu

--- app/src/main/java/com/talde3/app/loginORegistro.java
+++ app/src/main/java/com/talde3/app/loginORegistro.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
@@ -21,42 +22,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class loginORegistro extends AppCompatActivity {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_o_registro);
-
-        Map<String, Object> city = new HashMap<>();
-        city.put("name", "Los Angeles");
-        city.put("state", "CA");
-        city.put("country", "USA");
-
-        db.collection("cities").document("LA")
-                .set(city)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully written!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error writing document", e);
-                    }
-                });
-        Map<String, Object> data = new HashMap<>();
-        data.put("capital", true);
-
-        db.collection("cities").document("BJ")
-                .set(data, SetOptions.merge());
 
         Button loginButton = findViewById(R.id.btnLoginLogin);
         TextView txtRegistrarseLogin = findViewById(R.id.txtRegistrarseLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mAuth = FirebaseAuth.getInstance();
+
                 Intent intent = new Intent(loginORegistro.this, primeraPantalla.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/talde3/app/primeraPantalla.java
+++ app/src/main/java/com/talde3/app/primeraPantalla.java
@@ -3,6 +3,9 @@ package com.talde3.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class primeraPantalla extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class primeraPantalla extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primera_pantalla);
+
+        Button btnPrueba = findViewById(R.id.botonPrueba);
+        LinearLayout linearLayout = findViewById(R.id.linearLayoutMenu);
+        btnPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
